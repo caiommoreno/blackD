b57fb9ea31f5c7c6e97e8d84254e1fcae38fc2eb
Caio Moreno <caiommax@hotmail.com>
2020-09-29 16:59:51 -0300

    atualização

--- blackD/core/views.py
+++ blackD/core/views.py
@@ -24,6 +24,7 @@ def display_sales(request):
     }
     return render(request, 'sales.html', context)
 
+
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
