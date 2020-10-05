f05524c42d78eefc7710bc4c19df8495cfd49cef
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-06 00:06:56 +0300

    Update views.py

--- blackD/users/views.py
+++ blackD/users/views.py
@@ -77,9 +77,9 @@ class UsersV(View):
         else:
             u_form = UserUpdateForm(instance=request.user)
             p_form = ProfileUpdateForm(instance=request.user.profile)
-            context = {
-                'u_form': u_form,
-                'p_form': p_form
-            }
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
 
         return render(request, 'users/profile.html', context)
