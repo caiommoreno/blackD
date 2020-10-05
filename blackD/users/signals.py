809a79ec95cd1421d5023985e1a2489d2de35793
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-06 00:25:49 +0300

    Update signals.py

--- blackD/users/signals.py
+++ blackD/users/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-
+from django.contrib.auth.signals import user_logged_out
+import datetime
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -13,3 +14,8 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(user_logged_out)
+def save_last_login(sender, user, **kwargs):
+	user.profile.last_logout = datetime.datetime.now()
+	user.profile.save()
