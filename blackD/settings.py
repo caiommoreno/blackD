d3c5222580a74e996128dd3179122c8424ce91fc
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-05 23:55:00 +0300

    media folder added

--- blackD/settings.py
+++ blackD/settings.py
@@ -124,5 +124,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
