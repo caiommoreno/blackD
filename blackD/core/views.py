a1970c0e051bfbfaa4b150ecea0f812a564e6926
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-05 14:10:32 +0300

    login required

--- blackD/core/views.py
+++ blackD/core/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 from blackD.core.forms import ProductForm, SaleForm
 from blackD.core.models import Product, Sale
 
-
+@login_required
 def home(request):
     return render(request, 'index.html')
 
-
+@login_required
 def display_products(request):
     items = Product.objects.all()
     context = {
@@ -16,7 +16,7 @@ def display_products(request):
     }
     return render(request, 'products.html', context)
 
-
+@login_required
 def display_sales(request):
     items = Sale.objects.all()
     context = {
@@ -24,7 +24,7 @@ def display_sales(request):
     }
     return render(request, 'sales.html', context)
 
-
+@login_required
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -37,7 +37,7 @@ def add_product(request):
         form = ProductForm()
         return render(request, 'add_item.html', {'form': form, 'header': 'Adicionar Produtos'})
 
-
+@login_required
 def add_sales(request):
     if request.method == 'POST':
         form = SaleForm(request.POST)
@@ -50,7 +50,7 @@ def add_sales(request):
         form = SaleForm()
         return render(request, 'add_item.html', {'form': form, 'header': 'Adicionar Vendas'})
 
-
+@login_required
 def edit_product(request, pk):
     item = get_object_or_404(Product, pk=pk)
 
@@ -64,7 +64,7 @@ def edit_product(request, pk):
         form = ProductForm(instance=item)
         return render(request, 'edit_item.html', {'form': form, 'header': 'Editando Produtos'})
 
-
+@login_required
 def edit_sales(request, pk):
     item = get_object_or_404(Sale, pk=pk)
 
@@ -78,7 +78,7 @@ def edit_sales(request, pk):
         form = SaleForm(instance=item)
         return render(request, 'edit_item.html', {'form': form, 'header': 'Editando Vendas'})
 
-
+@login_required
 def delete_product(request, pk):
     Product.objects.filter(id=pk).delete()
 
@@ -89,7 +89,7 @@ def delete_product(request, pk):
     }
     return render(request, 'products.html', context)
 
-
+@login_required
 def delete_sales(request, pk):
     Sale.objects.filter(id=pk).delete()
 
