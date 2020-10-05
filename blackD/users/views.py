97216c00ac2a7d9c3808419f64580c066ca102bf
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-06 00:13:48 +0300

    Update views.py

--- blackD/users/views.py
+++ blackD/users/views.py
@@ -57,23 +57,18 @@ class UsersV(View):
     @login_required
     def profile(request):
         if request.method == 'POST':
-            if request.POST.get('update') == '':                
-                u_form = UserUpdateForm(request.POST, instance=request.user)
-                p_form = ProfileUpdateForm(request.POST,
-                                        request.FILES,
-                                        instance=request.user.profile)
-                if u_form.is_valid() and p_form.is_valid():
-                    u_form.save()
-                    p_form.save()
-                    messages.success(request, f'Your account has been updated!')
-                    return redirect('profile')
+                          
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+            if u_form.is_valid() and p_form.is_valid():
+                u_form.save()
+                p_form.save()
+                messages.success(request, f'Your account has been updated!')
+                return redirect('profile')
                
-            if request.POST.get('staff') == '':
-                user = request.user   
-                user.profile.staff_requested = True
-                user.save()
-                u_form = UserUpdateForm(instance=request.user)
-                p_form = ProfileUpdateForm(instance=request.user.profile)
+            
             context = {
                 'u_form': u_form,
                 'p_form': p_form
