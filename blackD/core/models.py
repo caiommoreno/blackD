32bbabef61b3bbeba69439758129c74fc344fc77
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-05 19:41:53 +0300

    Update models.py

--- blackD/core/models.py
+++ blackD/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    #user = models.OneToOneField(User, on_delete=models.CASCADE) 
     nome = models.CharField(max_length=100, blank=False, )
     categoria = models.CharField(max_length=100, blank=False)
     preco_custo = models.CharField(max_length=100)
@@ -20,7 +20,7 @@ class Product(models.Model):
 
 
 class Sale(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
     data = models.CharField(max_length=100, blank=False, )
     cliente = models.CharField(max_length=100, blank=False)
     total = models.CharField(max_length=100)
