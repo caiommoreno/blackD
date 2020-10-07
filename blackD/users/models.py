7e155681118382a10cc0dc55140391389bf5cb78
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-07 19:29:08 +0300

    Update models.py

--- blackD/users/models.py
+++ blackD/users/models.py
@@ -2,22 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     image = models.ImageField(default='default.png', upload_to='profile_pics')
-#     sex = models.CharField(max_length=6, blank=True, null=True)
-    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    sex = models.CharField(max_length=6, blank=True, null=True)
+    last_logout = models.DateField(default='1970-01-01', blank=True, null=True)
    
 
-#     def __str__(self):
-#         return f'{self.user.username} Profile'
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
-#     def save(self, **kwargs):
-#         super().save()
+    def save(self, **kwargs):
+        super().save()
 
-#         img = Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
-#         if img.height > 300 or img.width > 300:
-#             output_size = (300, 300)
-#             img.thumbnail(output_size)
-#             img.save(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
