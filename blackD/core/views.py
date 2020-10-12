ab2d2996102e46f0c0c046a6d6f0dc284c9bfac6
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-12 14:50:10 +0300

    Update views.py

--- blackD/core/views.py
+++ blackD/core/views.py
@@ -122,7 +122,7 @@ def add_sales(request):
         user = request.user
 
         data = request.POST.get('data')
-        data = datetime.strptime(data)
+        data = datetime.strptime('%d/%m/%y',data)
         cliente = request.POST.get('cliente')
         total = request.POST.get('total')
 
