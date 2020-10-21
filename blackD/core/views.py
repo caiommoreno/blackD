ace2a4a82e0dd12e6620906d444d671bd3215183
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-21 15:43:53 +0300

    Update views.py

--- blackD/core/views.py
+++ blackD/core/views.py
@@ -128,7 +128,7 @@ def home(request):
     else:
         # messages.warning(request, f"Desculpe, você precisa adicionar items na aba de 'Vendas' para usar o gráfico.")
         # return redirect('display_sales')
-        year = datetime.datetime.now()
+        year = datetime.now()
         year = year.year
         yYear = [year,]
         y = year
