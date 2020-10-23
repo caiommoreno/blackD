13d1177bb90e41866531c9bc4e544c81c2cfdfea
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-10-23 12:40:31 +0300

    Update main.js

--- blackD/core/static/assets/js/main.js
+++ blackD/core/static/assets/js/main.js
@@ -1,14 +1,6 @@
-function toTitleCase(string){
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
 function setActive() {
-	var pageName = window.location.href;
-	var activePage = window.location.href;
-	activePage=activePage.split("/", 20);
-	activePage=activePage[activePage.length -2];	
+	var activePage = window.location.href.split("/", 20)[3];	
 	var btn = document.getElementById(activePage);
 	btn.className += ' active'; 
-
- 	console.log(pageName)
 };
 
