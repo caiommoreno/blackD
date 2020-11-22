d0b771fd7f039160d73f088474e3d085e52e9d1f
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-11-22 21:32:03 +0200

    Update Calendar.py

--- blackD/core/Calendar.py
+++ blackD/core/Calendar.py
@@ -4,13 +4,14 @@ from datetime import datetime
 class clndr():
 	def year(self, x):
 		yy = x
-		print(calendar.calendar(yy))
+		year = calendar.calendar(yy)
+		return year
 
 
 	def month(self, x, z):
 		yy = x or datetime.now().year
 		mm = z or datetime.now().month
-		
-		print(calendar.month(yy, mm))  
+		month = calendar.month(yy, mm)
+		return month
 clndr = clndr()
 clndr.month(0, 0)
