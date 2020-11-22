fa81bc7ff4548da6f9962a014d740e8aa06f5c5d
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-11-22 21:53:32 +0200

    Update views.py

--- blackD/core/views.py
+++ blackD/core/views.py
@@ -365,3 +365,31 @@ def delete_product(request, pk):
 @login_required
 def under_construct(request):
     return render(request, 'constructing.html')
+
+
+@login_required
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'cal/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+
+@login_required
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
