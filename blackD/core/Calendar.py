2f70225bdf1c125a27223ac703ae23824cc3532a
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-11-22 21:49:55 +0200

    Update Calendar.py

--- blackD/core/Calendar.py
+++ blackD/core/Calendar.py
@@ -1,18 +1,40 @@
-import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from .models import Event
 
-class clndr(HTMLCalendar):
-	def year(self, x):
-		yy = x
-		year = calendar.calendar(yy)
-		return year
+class Calendar(HTMLCalendar):
+	def __init__(self, year=None, month=None):
+		self.year = year
+		self.month = month
+		super(Calendar, self).__init__()
 
+	# formats a day as a td
+	# filter events by day
+	def formatday(self, day, events):
+		events_per_day = events.filter(start_time__day=day)
+		d = ''
+		for event in events_per_day:
+			d += f'<li> {event.title} </li>'
 
-	def month(self, x, z):
-		yy = x or datetime.now().year
-		mm = z or datetime.now().month
-		month = calendar.month(yy, mm)
-		return month
-clndr = clndr()
-clndr.month(0, 0)
+		if day != 0:
+			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
+		return '<td></td>'
+
+	# formats a week as a tr 
+	def formatweek(self, theweek, events):
+		week = ''
+		for d, weekday in theweek:
+			week += self.formatday(d, events)
+		return f'<tr> {week} </tr>'
+
+	# formats a month as a table
+	# filter events by year and month
+	def formatmonth(self, withyear=True):
+		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
+
+		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+		cal += f'{self.formatweekheader()}\n'
+		for week in self.monthdays2calendar(self.year, self.month):
+			cal += f'{self.formatweek(week, events)}\n'
+		return cal
