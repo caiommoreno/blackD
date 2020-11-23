b65c9f39100644e09c97437028279f3828056fd4
FerasAlfrih <43246222+FerasAlfrih@users.noreply.github.com>
2020-11-23 10:05:34 +0200

    Update models.py

--- blackD/core/models.py
+++ blackD/core/models.py
@@ -41,3 +41,6 @@ class Event(models.Model):
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
